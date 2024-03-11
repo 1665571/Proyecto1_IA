75898f9ef33619c583dee21ba57c07f26015606a
Alex Iglesias <165571@uab.cat>
2024-03-11 11:43:46 +0100

    Inicio Proyecto1 Parte2

--- Code/SearchAlgorithm.py
+++ Code/SearchAlgorithm.py
@@ -1,15 +1,12 @@
 # This file contains all the required routines to make an A* search algorithm.
 #
 __author__ = '1665571'
-__group__='DL.1030(ENG)'
 # _________________________________________________________________________________________
 # Intel.ligencia Artificial
 # Curs 2023 - 2024
 # Universitat Autonoma de Barcelona
 # _______________________________________________________________________________________
 
-from collections import OrderedDict
-from ctypes.wintypes import _COORD
 from SubwayMap import *
 from utils import *
 import os
@@ -34,7 +31,8 @@ def expand(path, map):
         path_list.append(auxPath)
         
     return path_list
-    
+    pass
+
 
 def remove_cycles(path_list):
     """
@@ -54,6 +52,7 @@ def remove_cycles(path_list):
         if not cicle:
             noCycleList.append(path)
     return noCycleList
+    pass
 
 
 def insert_depth_first_search(expand_paths, list_of_path):
@@ -81,7 +80,6 @@ def depth_first_search(origin_id, destination_id, map):
         Returns:
             list_of_path[0] (Path Class): the route that goes from origin_id to destination_id
     """
-    
     stack=[Path([origin_id])]
     while len(stack)>0 and stack[0].last!=destination_id:
             head=stack[0]
@@ -256,7 +254,7 @@ def distance_to_stations(coord, map):
         From coordinates, it computes the distance to all stations in map.
         Format of the parameter is:
         Args:
-            coord (list):  Two REAL values, which refer to the coordinates of a point in the city.
+            coord (list): Two REAL values, which refer to the coordinates of a point in the city.
             map (object of Map class): All the map information
         Returns:
             (dict): Dictionary containing as keys, all the Indexes of all the stations in the map, and as values, the
@@ -288,5 +286,20 @@ def Astar(origin_id, destination_id, map, type_preference=0):
                             3 - minimum Transfers
         Returns:
             list_of_path[0] (Path Class): The route that goes from origin_id to destination_id
+    """
+    pass
+
+
+def Astar_improved(origin_coord, destination_coord, map):
+    """
+     A* Search algorithm
+     Format of the parameter is:
+        Args:
+            origin_coord (list): Two REAL values, which refer to the coordinates of the starting position
+            destination_coord (list): Two REAL values, which refer to the coordinates of the final position
+            map (object of Map class): All the map information
+
+        Returns:
+            list_of_path[0] (Path Class): The route that goes from origin_coord to destination_coord
     """
     pass
